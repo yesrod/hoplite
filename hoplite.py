5cd5849c01cad06226eed4a4e9c531dc6336ea13
Adam Dorsey <adam.dorsey@gmail.com>
2018-03-20 18:28:43 +0000

    Properly handle names and weights of kegs
    Display beer name as configured
    Add dictionary of weights, associate with each channel
    Add function to center align text on LCD screen

--- hoplite.py
+++ hoplite.py
@@ -6,6 +6,16 @@ import RPi.GPIO as GPIO
 import time, sys, os, json
 from hx711 import HX711
 
+# keg data dictionary
+# value is list( volume in liters, empty weight in kg )
+keg_data = {
+'half_bbl': (58.6, 13.6),
+'tall_qtr_bbl': (29.3, 10),
+'short_qtr_bbl': (29.3, 10),
+'sixth_bbl': (19.5, 7.5),
+'corny': (18.9, 4),
+}
+
 def cleanAndExit():
     print "Cleaning..."
     GPIO.cleanup()
@@ -68,23 +78,30 @@ def save_config(config):
         print "Could not save config: %s" % e.strerror
 
 def build_config():
-    print "building new config"
     config = dict()
     config['hx711_1'] = dict()
     config['hx711_1']['chA'] = dict()
     config['hx711_1']['chB'] = dict()
     config['hx711_1']['chA']['offset'] = None
     config['hx711_1']['chA']['refunit'] = 21.7
+    config['hx711_1']['chA']['keg_name'] = "Yuengling"
+    config['hx711_1']['chA']['keg_size'] = keg_data['half_bbl']
     config['hx711_1']['chB']['offset'] = None
     config['hx711_1']['chB']['refunit'] = 5.4
+    config['hx711_1']['chB']['keg_name'] = "Angry Orchard"
+    config['hx711_1']['chB']['keg_size'] = keg_data['sixth_bbl']
     config['hx711_1']['dout'] = 5
     config['hx711_1']['pd_sck'] = 6
     return config
 
-def text_center_x(device, draw, y, message, fill="white"):
+def text_header(device, draw, y, message, fill="white"):
     W = device.width
     w, h = draw.textsize(message)
     draw.text(((W-w)/2, y), message, fill=fill)
+
+def text_align_center(device, draw, x, y, message, fill="white"):
+    w, h = draw.textsize(message)
+    draw.text(((x-w)/2, y), message, fill=fill)
 
 def fill_bar(device, draw, x, y, min_w, max_w, w, outline="white", fill="red"):
     trim_w = max(min_w, min(w, max_w))
@@ -96,6 +113,10 @@ def fill_bar(device, draw, x, y, min_w, max_w, w, outline="white", fill="red"):
     draw.rectangle([x,y, x+20,device.height-20], outline=outline, fill="black")
     draw.rectangle([x+1,fill_height, x+19,max_y], outline=fill, fill=fill)
 
+def as_kg(val):
+    return "%s kg" % str(val / 1000.0)
+
+print keg_data
 
 config = load_config()
 
@@ -106,23 +127,32 @@ hx = init_hx711(config)
 
 hx.power_down()
 
+chA_name = config['hx711_1']['chA']['keg_name']
+chB_name = config['hx711_1']['chB']['keg_name']
+chA_min = config['hx711_1']['chA']['keg_size'][1] * 1000
+chB_min = config['hx711_1']['chB']['keg_size'][1] * 1000
+chA_max = chA_min + ( config['hx711_1']['chA']['keg_size'][0] * 1000 )
+chB_max = chB_min + ( config['hx711_1']['chB']['keg_size'][0] * 1000 ) 
+
 while True:
     try:
         hx.power_up()
 
-        ch1 = hx711_read_chA(hx)
-        ch2 = hx711_read_chB(hx)
-        print "ch1: %s g  ch2: %s g" % ( str(ch1), str(ch2) )
+        chA = hx711_read_chA(hx)
+        chB = hx711_read_chB(hx)
+        print "%s: %s/%s  %s: %s/%s" % ( chA_name, as_kg(chA), as_kg(chA_max), chB_name, as_kg(chB), as_kg(chB_max) )
+        print "min: %s %s" % ( chA_min, chB_min )
 
         with canvas(device) as draw:
-            #draw.rectangle(device.bounding_box, outline="white", fill="black")
-            text_center_x(device, draw, 0, "HOPLITE", fill="red")
+            text_header(device, draw, 0, "HOPLITE", fill="red")
 
-            fill_bar(device, draw, 30, 20, 0, 2000, ch1)
-            draw.text((30, device.height-10), "%s g" % str(ch1), fill="white")
+            text_align_center(device, draw, 30, 10, chA_name)
+            fill_bar(device, draw, 30, 20, chA_min, chA_max, chA)
+            draw.text((30, device.height-10), as_kg(chA), fill="white")
 
-            fill_bar(device, draw, 110, 20, 0, 2000, ch2)
-            draw.text((100, device.height-10), "%s g" % str(ch2), fill="white")
+            text_align_center(device, draw, 30, 10, chB_name)
+            fill_bar(device, draw, 110, 20, chB_min, chB_max, chB)
+            draw.text((100, device.height-10), as_kg(chB), fill="white")
 
         hx.power_down()
         time.sleep(5)
