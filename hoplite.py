7cd38c7b9dff6a46c008723db19f01789dfdc0eb
Adam Dorsey <adam.dorsey@gmail.com>
2018-03-21 16:05:25 +0000

    Complete moving hoplite to a class.
    It wasn't just as simple as adding class hoplite(): at the top and patting
    myself on the back, apparently.
    
    Also moved reading weight data and rendering to their own functions.  This
    will help when web interface comes; that script will become responsible for
    updating weight data and sending results to the LCD regularly.

--- hoplite.py
+++ hoplite.py
@@ -9,9 +9,10 @@ import json
 from hx711 import HX711
 
 
-class hoplite:
+class hoplite():
     # keg data dictionary
     # value is list( volume in liters, empty weight in kg )
+    global keg_data
     keg_data = {
         'half_bbl': (58.6, 13.6),
         'tall_qtr_bbl': (29.3, 10),
@@ -20,8 +21,28 @@ class hoplite:
         'corny': (18.9, 4),
     }
     
+    # config
+    global config
 
-    def init_st7735():
+    # output device and draw handle
+    global device
+    global draw
+
+    # hx711_1, for keg weighting
+    global hx
+    global chA
+    global chB
+
+    def __init__(self):
+        self.config = self.load_config()
+
+        self.device = self.init_st7735()
+
+        self.hx = self.init_hx711()
+        self.hx.power_down()
+
+
+    def init_st7735(self):
         light = backlight(gpio=GPIO, gpio_LIGHT=18, active_low=False)
         light.enable(True)
         serial = spi(port=0, device=0, gpio_DC=23, gpio_RST=24)
@@ -29,13 +50,13 @@ class hoplite:
         return device
 
     
-    def init_hx711(config):
-        dout = config['hx711_1']['dout']
-        pd_sck = config['hx711_1']['pd_sck']
-        offset_A = config['hx711_1']['chA']['offset']
-        refunit_A = config['hx711_1']['chA']['refunit']
-        offset_B = config['hx711_1']['chB']['offset']
-        refunit_B = config['hx711_1']['chB']['refunit']
+    def init_hx711(self):
+        dout = self.config['hx711_1']['dout']
+        pd_sck = self.config['hx711_1']['pd_sck']
+        offset_A = self.config['hx711_1']['chA']['offset']
+        refunit_A = self.config['hx711_1']['chA']['refunit']
+        offset_B = self.config['hx711_1']['chB']['offset']
+        refunit_B = self.config['hx711_1']['chB']['refunit']
         hx = HX711(dout, pd_sck)
         hx.set_reading_format("LSB", "MSB")
         hx.set_reference_unit_A(refunit_A)
@@ -45,134 +66,136 @@ class hoplite:
             hx.set_offset_A(offset_A)
         else:
             hx.tare_A()
-            config['hx711_1']['chA']['offset'] = hx.OFFSET_A
+            self.config['hx711_1']['chA']['offset'] = hx.OFFSET_A
         if offset_B:
             hx.set_offset_B(offset_B)
         else:
             hx.tare_B()
-            config['hx711_1']['chB']['offset'] = hx.OFFSET_B
+            self.config['hx711_1']['chB']['offset'] = hx.OFFSET_B
         return hx
 
     
-    def hx711_read_chA(hx):
+    def hx711_read_chA(self, hx):
         return int(hx.get_weight_A(5))
 
     
-    def hx711_read_chB(hx):
+    def hx711_read_chB(self, hx):
         return int(hx.get_weight_B(5))
 
     
-    def load_config():
+    def load_config(self):
         try: 
             save = open("config.json", "r")
-            config = json.load(save)
+            self.config = json.load(save)
             save.close()
         except (IOError, ValueError):
             print "no config found, using defaults"
-            config = build_config()
-        return config
+            self.config = self.build_config()
+        return self.config
 
     
-    def save_config(config):
+    def save_config(self, config):
         try:
             save = open("config.json", "w")
-            json.dump(config, save, indent=2)
+            json.dump(self.config, save, indent=2)
             save.close()
         except IOError:
             print "Could not save config: %s" % e.strerror
 
     
-    def build_config():
-        config = dict()
-        config['hx711_1'] = dict()
-        config['hx711_1']['chA'] = dict()
-        config['hx711_1']['chB'] = dict()
-        config['hx711_1']['chA']['offset'] = None
-        config['hx711_1']['chA']['refunit'] = 21.7
-        config['hx711_1']['chA']['keg_name'] = "Yuengling"
-        config['hx711_1']['chA']['keg_size'] = keg_data['half_bbl']
-        config['hx711_1']['chB']['offset'] = None
-        config['hx711_1']['chB']['refunit'] = 5.4
-        config['hx711_1']['chB']['keg_name'] = "Angry Orchard"
-        config['hx711_1']['chB']['keg_size'] = keg_data['sixth_bbl']
-        config['hx711_1']['dout'] = 5
-        config['hx711_1']['pd_sck'] = 6
+    def build_config(self):
+        self.config = dict()
+        self.config['hx711_1'] = dict()
+        self.config['hx711_1']['chA'] = dict()
+        self.config['hx711_1']['chB'] = dict()
+        self.config['hx711_1']['chA']['offset'] = None
+        self.config['hx711_1']['chA']['refunit'] = 21.7
+        self.config['hx711_1']['chA']['keg_name'] = "Yuengling"
+        self.config['hx711_1']['chA']['keg_size'] = keg_data['half_bbl']
+        self.config['hx711_1']['chB']['offset'] = None
+        self.config['hx711_1']['chB']['refunit'] = 5.4
+        self.config['hx711_1']['chB']['keg_name'] = "Angry Orchard"
+        self.config['hx711_1']['chB']['keg_size'] = keg_data['sixth_bbl']
+        self.config['hx711_1']['dout'] = 5
+        self.config['hx711_1']['pd_sck'] = 6
         return config
 
     
-    def text_header(device, draw, y, message, fill="white"):
-        W = device.width
-        w, h = draw.textsize(message)
-        draw.text(((W-w)/2, y), message, fill=fill)
+    def text_header(self, y, message, fill="white"):
+        W = self.device.width
+        w, h = self.draw.textsize(message)
+        self.draw.text(((W-w)/2, y), message, fill=fill)
 
     
-    def text_align_center(device, draw, x, y, message, fill="white"):
-        w, h = draw.textsize(message)
-        draw.text((x-(w/2), y), message, fill=fill)
+    def text_align_center(self, x, y, message, fill="white"):
+        w, h = self.draw.textsize(message)
+        self.draw.text((x-(w/2), y), message, fill=fill)
 
     
-    def fill_bar(device, draw, x, y, min_w, max_w, w, outline="white", fill="red"):
+    def fill_bar(self, x, y, min_w, max_w, w, outline="white", fill="red"):
         net_w = max(w - min_w, 0)
         max_net_w = max_w - min_w
         fill_percent = float(net_w) / float(max_net_w)
-        max_y = device.height - 21
+        max_y = self.device.height - 21
         min_y = y+1
         max_bar = max_y - min_y
         fill_height = min_y + (max_bar - (max_bar * fill_percent))
-        draw.rectangle([x,y, x+20,device.height-20], outline=outline, fill="black")
-        draw.rectangle([x+1,fill_height, x+19,max_y], outline=fill, fill=fill)
+        self.draw.rectangle([x,y, x+20,self.device.height-20], outline=outline, fill="black")
+        self.draw.rectangle([x+1,fill_height, x+19,max_y], outline=fill, fill=fill)
 
     
-    def as_kg(val):
+    def as_kg(self, val):
         return "%s kg" % "{0:.2f}".format(val / 1000.0)
-    
 
-    def main():
-        print keg_data
-        
-        config = load_config()
-        
-        print config
-        
-        device = init_st7735()
-        hx = init_hx711(config)
-        
-        hx.power_down()
-        
-        chA_name = config['hx711_1']['chA']['keg_name']
-        chB_name = config['hx711_1']['chB']['keg_name']
-        chA_min = config['hx711_1']['chA']['keg_size'][1] * 1000
-        chB_min = config['hx711_1']['chB']['keg_size'][1] * 1000
-        chA_max = chA_min + ( config['hx711_1']['chA']['keg_size'][0] * 1000 )
-        chB_max = chB_min + ( config['hx711_1']['chB']['keg_size'][0] * 1000 ) 
+
+    def read_weight(self):
+        self.hx.power_up()
+        self.chA = self.hx711_read_chA(self.hx)
+        self.chB = self.hx711_read_chB(self.hx)
+        self.hx.power_down()
+
+
+    def render_st7735(self):
+        chA_name = self.config['hx711_1']['chA']['keg_name']
+        chB_name = self.config['hx711_1']['chB']['keg_name']
+        chA_min = self.config['hx711_1']['chA']['keg_size'][1] * 1000
+        chB_min = self.config['hx711_1']['chB']['keg_size'][1] * 1000
+        chA_max = chA_min + ( self.config['hx711_1']['chA']['keg_size'][0] * 1000 )
+        chB_max = chB_min + ( self.config['hx711_1']['chB']['keg_size'][0] * 1000 )
+
+        print "%s: %s/%s  %s: %s/%s" % ( chA_name, self.chA, chA_max, 
+                                         chB_name, self.chB, chB_max )
+        print "min: %s %s" % ( chA_min, chB_min )
+
+        self.text_header(0, "HOPLITE", fill="red")
+
+        self.text_align_center(40, 10, chA_name)
+        self.fill_bar(30, 20, chA_min, chA_max, self.chA)
+        self.text_align_center(40, self.device.height-10, self.as_kg(self.chA))
+
+        self.text_align_center(120, 10, chB_name)
+        self.fill_bar(110, 20, chB_min, chB_max, self.chB)
+        self.text_align_center(120, self.device.height-10, self.as_kg(self.chB))
+
+
+    def main(self):
         
         while True:
             try:
-                hx.power_up()
+                self.read_weight()
+
+                with canvas(self.device) as self.draw:
+                    self.render_st7735()
         
-                chA = hx711_read_chA(hx)
-                chB = hx711_read_chB(hx)
-                print "%s: %s/%s  %s: %s/%s" % ( chA_name, chA, chA_max, chB_name, chB, chB_max )
-                print "min: %s %s" % ( chA_min, chB_min )
-        
-                with canvas(device) as draw:
-                    text_header(device, draw, 0, "HOPLITE", fill="red")
-        
-                    text_align_center(device, draw, 40, 10, chA_name)
-                    fill_bar(device, draw, 30, 20, chA_min, chA_max, chA)
-                    text_align_center(device, draw, 40, device.height-10, as_kg(chA))
-        
-                    text_align_center(device, draw, 120, 10, chB_name)
-                    fill_bar(device, draw, 110, 20, chB_min, chB_max, chB)
-                    text_align_center(device, draw, 120, device.height-10, as_kg(chB))
-        
-                hx.power_down()
                 time.sleep(5)
             except (KeyboardInterrupt, SystemExit):
-                save_config(config)
-                hx.power_down()
+                self.save_config(self.config)
+                self.hx.power_down()
                 GPIO.cleanup()
                 sys.exit()
 
-    if __name__ == "__main__":
-        main()        
+
+# this is here in case we get run as a standalone script
+if __name__ == '__main__':
+    h = hoplite()
+    h.main()
