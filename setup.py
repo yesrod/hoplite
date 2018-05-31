d0af432320294f53aae57008c045481e44f9334f
Adam Dorsey <adam.dorsey@gmail.com>
2018-05-31 19:46:53 +0000

    Add missing remi requirement to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='HOPLITE: A system for monitoring kegerator levels and temperatures',
 #    py_modules=['Hoplite', 'hoplite-web'],
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    install_requires=['Rpi.GPIO', 'numpy', 'luma.lcd', 'hx711', 'posix_ipc'],
+    install_requires=['Rpi.GPIO', 'numpy', 'luma.lcd', 'hx711', 'posix_ipc', 'remi'],
     package_dir={'hoplite': 'hoplite/'},
     package_data={  # Optional
         'hoplite': ['static/example-config.json', 'static/settings_16.png'],
