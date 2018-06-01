366a198422df62f5c6e8fbdb59508de5d51c14cf
Adam Dorsey <adam.dorsey@gmail.com>
2018-06-01 19:38:01 +0000

    Cleaned up setup.py with autopep8

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name='hoplite',
     version='1.0.5',
     description='HOPLITE: A system for monitoring kegerator levels and temperatures',
-#    py_modules=['Hoplite', 'hoplite-web'],
+    #    py_modules=['Hoplite', 'hoplite-web'],
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    install_requires=['Rpi.GPIO', 'numpy', 'luma.lcd', 'hx711', 'posix_ipc', 'remi'],
+    install_requires=[
+        'Rpi.GPIO', 'numpy', 'luma.lcd', 'hx711', 'posix_ipc', 'remi'],
     package_dir={'hoplite': 'hoplite/'},
     package_data={  # Optional
         'hoplite': ['static/example-config.json', 'static/settings_16.png'],
     },
 )
-
