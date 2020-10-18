9a41c8eee5848631824af77eb60c749b87b1bd4a
Adam Dorsey <adam.dorsey@gmail.com>
2020-10-18 17:02:13 -0400

    Require Python 3.5, warn about 3.5 being EOL

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 from setuptools import setup, find_packages
+import sys
+
+if not sys.version_info[0] == 3:
+    sys.exit("ERROR: HOPLITE requires Python 3.5 or higher")
+else:
+    if sys.version_info[1] < 5:
+        sys.exit("ERROR: HOPLITE requires Python 3.5 or higher")
+    if sys.version_info[1] == 5:
+        print("WARNING: Python 3.5 is end of life and will not be supported in the future.")
+        print("Please upgrade your Python version to 3.6 or higher,") 
+        print("either by upgrading your OS or installing Python manually.") 
 
 setup(
     name='hoplite',
@@ -6,6 +17,7 @@ setup(
     description='HOPLITE: A system for monitoring kegerator levels and temperatures',
     #    py_modules=['Hoplite', 'hoplite-web'],
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    python_requires=">=3.5"
     install_requires=[
         'Rpi.GPIO', 'numpy', 'luma.lcd>=2.0.0', 'hx711', 'posix_ipc', 'remi', 'flask'],
     package_dir={'hoplite': 'hoplite/'},
