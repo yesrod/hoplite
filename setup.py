9c6ea12a503e10d0a9dba8545fc2fd3539727dca
Adam Dorsey <adam.dorsey@gmail.com>
2020-10-18 17:06:13 -0400

    Hoplite v1.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='hoplite',
-    version='1.1.1',
+    version='1.2.0',
     description='HOPLITE: A system for monitoring kegerator levels and temperatures',
     #    py_modules=['Hoplite', 'hoplite-web'],
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
